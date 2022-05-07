6098b55aa11a2ca8b6b610eb3660d6e9220a568f
Leonardo Ruma <leonardo.ruma@gmail.com>
2022-05-07 21:24:41 +0100

    Adicionado metodo para remover todos os circulos.

--- script.js
+++ script.js
@@ -5,17 +5,27 @@ const CircleType = [
 ]
 
 const divTools = document.createElement("div");
+divTools.classList.add("div-tools");
 
 const buttonAddCircles = document.createElement("button");
+buttonAddCircles.id = "buttonAddCircles";
 buttonAddCircles.classList.add("button");
 buttonAddCircles.type = "button";
 buttonAddCircles.onclick = () => addCircle();
-buttonAddCircles.innerHTML="Adicionar Circulos"
+buttonAddCircles.innerHTML="Add Circles";
+
+const buttonRemoveCircles = document.createElement("button");
+buttonRemoveCircles.id = "buttonRemoveCircles";
+buttonRemoveCircles.classList.add("button");
+buttonRemoveCircles.type = "button";
+buttonRemoveCircles.onclick = () => this.deleteAllCircles();
+buttonRemoveCircles.innerHTML="Remove Circles";
 
 const inputNumberCircles = document.createElement("input");
 inputNumberCircles.id = "inputNumberCircles"
 inputNumberCircles.classList.add("input");
 
+divTools.appendChild(buttonRemoveCircles);
 divTools.appendChild(buttonAddCircles);
 divTools.appendChild(inputNumberCircles);
 
@@ -30,7 +40,7 @@ function addCircle() {
     for (let i = circlesNumber; i > 0; i--) {
         let circle = document.createElement("div");
         circle.classList.add("circle")
-        circle.id = i.toString();
+        circle.id = `${i}-${Math.random()*Math.random()}`
         circle.onclick = (event) => this.disableCircle(event);
         circle.classList.add(CircleType[Math.floor(Math.random()*CircleType.length)]);
         document.body.appendChild(circle);
@@ -40,4 +50,12 @@ function addCircle() {
 
 function disableCircle(event) {
     event.target.style.display = "none";
+}
+
+function deleteAllCircles() {
+    const circles = document.querySelectorAll('.circle');
+    circles.forEach(circle => {
+       circle.remove();
+    });
+    alert("all circles have been removed")
 }
